be1cabef04f0c14ecde6b8b33809940bb63a2026
StevenMacias <stevenmcs22@gmail.com>
2024-06-26 09:42:03 -0400

    init commit

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    hashedPassword: {
+        type: String,
+        required: true,
+    }
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.hashedPassword;
+    }
+});
+
+module.exports = new mongoose.model('User', userSchema);
---
+++ server.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+
+const profilesRouter = require('./controllers/profiles');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.json());
+app.use(cors());
+// Routes go here
+app.use('/test-jwt', testJWTRouter);
+app.use('/users', usersRouter);
+app.use('/profiles', profilesRouter);
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
